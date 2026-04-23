bd5380e0e62e07e886baa60a40c3a58bc1a9737e
Zypixh <moying8259@gmail.com>
2026-04-23 22:01:59 +0800

    Remove TOA daemon artifacts from release

--- toa-sender/daemon/target/debug/build/quote-6c21111b1381442e/build_script_build-6c21111b1381442e.d
+++
@@ -1,5 +0,0 @@
-/Users/moying/Documents/project/cloud-node-rust/toa-sender/daemon/target/debug/build/quote-6c21111b1381442e/build_script_build-6c21111b1381442e.d: /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/moying/Documents/project/cloud-node-rust/toa-sender/daemon/target/debug/build/quote-6c21111b1381442e/build_script_build-6c21111b1381442e: /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
--- toa-sender/daemon/target/debug/deps/clap_lex-0c7b87e1f80a9daa.d
+++
@@ -1,6 +0,0 @@
-/Users/moying/Documents/project/cloud-node-rust/toa-sender/daemon/target/debug/deps/clap_lex-0c7b87e1f80a9daa.d: /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/lib.rs /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/ext.rs
-
-/Users/moying/Documents/project/cloud-node-rust/toa-sender/daemon/target/debug/deps/libclap_lex-0c7b87e1f80a9daa.rmeta: /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/lib.rs /Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/ext.rs
-
-/Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/lib.rs:
-/Users/moying/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-1.1.0/src/ext.rs:
