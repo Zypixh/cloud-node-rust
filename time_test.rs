0b90c53147f00178d3b44e164c310aa40a42cc92
Zypixh <moying8259@gmail.com>
2026-05-23 16:32:59 +0800

    chore(repo): 移除本地运行数据和临时文件

--- time_test.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("UTC: {}", chrono::Utc::now());
-    println!("Local: {}", chrono::Local::now());
-    println!("Local iso: {}", chrono::Local::now().format("%Y-%m-%dT%H:%M:%S.000%:z"));
-}
